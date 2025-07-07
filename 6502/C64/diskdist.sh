2bccafd507a3da14f7384d064e92e698b30c85f6
N. Haedecke <webmaster@ufud.org>
2025-07-07 19:58:55 +0200

    rename target compiler base file

--- 6502/C64/diskdist.sh
+++ 6502/C64/diskdist.sh
@@ -13,8 +13,8 @@ c1541 v4th-3.9.6_1.d64 -write cbmfiles/v4th-c16+
 c1541 v4th-3.9.6_1.d64 -write cbmfiles/v4th-x16
 c1541 v4th-3.9.6_1.d64 -write cbmfiles/v4th-x16e
 
-c1541 v4th-3.9.6_1.d64 -write ~/src/vi65-code/trunk/bin/vi65_c64_64 vi65-c64-64col
-c1541 v4th-3.9.6_1.d64 -write ~/src/vi65-code/trunk/bin/vi65_c16_40 vi65-c16-40col
-c1541 v4th-3.9.6_1.d64 -write ~/src/vi65-code/trunk/bin/vi65_plus4_64 vi65-plus4-64col
+c1541 v4th-3.9.6_1.d64 -write contrib/vi65_c64_64 vi65-c64-64col
+c1541 v4th-3.9.6_1.d64 -write contrib/vi65_c16_40 vi65-c16-40col
+c1541 v4th-3.9.6_1.d64 -write contrib/vi65_plus4_64 vi65-plus4-64col
 
 for i in $(ls cbmfiles/*.fth | grep -v grep | grep -v 'vf-' | grep -v 'v4th') ; do c1541 v4th-3.9.6_2.d64 -write $i `basename $i`,s ; done
